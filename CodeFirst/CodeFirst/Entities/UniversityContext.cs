138c2ad9a9ed45bc3b148730ff4141b236c583f7
danjab-jd <jd@pja.edu.pl>
2022-05-05 15:25:42 +0200

    CF Migration

---
+++ CodeFirst/CodeFirst/Entities/UniversityContext.cs
@@ -0,0 +1,26 @@
+﻿using CodeFirst.Entities.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace CodeFirst.Entities
+{
+    public class UniversityContext : DbContext
+    {
+        public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<Group> Groups { get; set; }
+        public virtual DbSet<StudentGroup> StudentGroups { get; set; }
+
+        public UniversityContext()
+        { }
+
+        public UniversityContext(DbContextOptions<UniversityContext> options) : base(options) 
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(
+                typeof(StudentEfConfiguration).GetTypeInfo().Assembly);
+        }
+
+    }
+}
